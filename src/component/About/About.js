eed18059250843d39e7fb5b7cd65dbd66aa369c1
prajktamhase <mhaseprajkta@gmail.com>
2023-11-07 13:27:45 +0800

    bykerentel project

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,7 +1,5 @@
 import React from "react"
 import "./About.css";
-import Prajkta from "./pm.png"
-
 const About = () => {
   return (
     <>
@@ -10,17 +8,8 @@ const About = () => {
           <h1 className="text-light text-center ms-2 mt-5">What People Are Saying<br /> About Us</h1><br />
           <h3 className="text-light fs-4 ms-2 mb-5">We provide our bast service our client always and <br />try to achieve our client trust....!</h3>
         </div>
-
-
       </div>
-
-
-    
-
-
         </>
     )
-
 }
-
 export default About;
